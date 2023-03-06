521eacecf483adc0528d7c45adbcb3af0d8fe0b0
Viking <djviking@yandex.ru>
2023-03-06 22:39:54 +0300

    empl in browser

---
+++ EmployeeNotFoundException.java
@@ -0,0 +1,8 @@
+package pro.sky.employe25.employeers.exceprion;
+
+public class EmployeeNotFoundException extends RuntimeException {
+    public EmployeeNotFoundException(String message) {
+        super(message);
+
+    }
+}
