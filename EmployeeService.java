3402ee3fbc0a282f789be18401fe162a924a8fe3
Viking <djviking@yandex.ru>
2023-03-12 23:34:04 +0300

    empl in browser

--- EmployeeService.java
+++ EmployeeService.java
@@ -2,8 +2,7 @@ package pro.sky.employe25.employeers.exceprion;
 
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class EmployeeService {
@@ -11,21 +10,21 @@ public class EmployeeService {
     // устанавливаем лимит на сотрудников, пусть их будет 3
     private final int maxEmployersNumbers = 3;
     // переводим в лист формат
-    private final List<Employee> employees = new ArrayList<>();
+    private final Map<Integer, Employee> employeeByHashCode = new HashMap<>();
 
     // метод на добавление сотрудника
     public Employee add(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
 
-        if (employees.size() == maxEmployersNumbers) {
+        if (employeeByHashCode.size() == maxEmployersNumbers) {
             // прокидываем ошибку если слишком много людкй
             throw new EmployeeStorageIsFullException(" Людей слишком много");
         }
         // прокидываем на ошибку если не нуль и если есть такой тип и переводим в лист формат
-        if (employees.contains(employee)) {
+        if (employeeByHashCode.containsKey(employee.hashCode())) {
             throw new EmployeeAlreadyAddedException(" такой тип уже есть");
         }
-        employees.add(employee);
+        employeeByHashCode.put(employee.hashCode(), employee);
 
 //        for (int i = 0; i < employees.size(); i++) {
 //            if (employees[i] != null && employees[i].equals(employee)) {
@@ -43,12 +42,11 @@ public class EmployeeService {
     // метод на поиск сотрудника
     public Employee find(String firstName, String lastName) {
 
-        Employee employee = null;
-        for (Employee e : employees) {
-            if (e != null && firstName.equals(e.getFirstNameEmployer()) && lastName.equals(e.getLastNameEmployer())) {
-                employee = e;
-            }
-        }
+  //      Employee employeeForSearch =  new Employee();
+        int employeeHashCode = Objects.hash(firstName, lastName);
+
+        Employee employee = employeeByHashCode.get(employeeHashCode);
+
         if (employee == null) {
             throw new EmployeeNotFoundException(" при поиске сотрудник не найден.");
         }
